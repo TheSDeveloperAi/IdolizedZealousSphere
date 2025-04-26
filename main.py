bca319ce38f57f8f366c20780ff4827f6d199965
GUILHERME GOMES DE SENA <sena.america@gmail.com>
2025-04-26 13:11:30 +0100

    renamed the repository, making a test for future updates

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from dataclasses import dataclass
-import pandas as pd  #test6
+import pandas as pd  #test7
 from typing import Dict, List, Tuple, Optional
 # ---------------------------
 # Global Pricing Information
